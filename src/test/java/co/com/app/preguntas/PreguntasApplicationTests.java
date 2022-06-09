6f891bf309e8c2ffeb8a65d8cdff61f34214974e
juantoro7295 <juanpatoro2011@gmail.com>
2022-06-09 13:01:30 -0500

    Proyecto esqueleto - Backend

---
+++ src/test/java/co/com/app/preguntas/PreguntasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.app.preguntas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
